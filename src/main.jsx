013abe18d0951a1d6cf3469d45ae1cc3afe454cc
jpnvidaltech-cmd <jpnvidal.tech@gmail.com>
2026-03-26 13:04:10 -0300

    Actualizar src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
+/**
+ * @file main.jsx
+ * @description Punto de entrada principal para la SPA de la inmobiliaria.
+ * Renderiza la aplicación React en el elemento root de index.html e inicializa los estilos básicos.
+ */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.jsx'
 import './index.css'
 
+/**
+ * Inicialización de la aplicación React con el componente App.
+ * El uso de StrictMode ayuda a identificar problemas potenciales en el código durante el desarrollo.
+ */
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  )
+  </React.StrictMode>,
+)
